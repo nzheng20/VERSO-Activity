d80a183f976c63a7294b521f1127e3bc5ed58e20
abbyvece <122999865+abbyvece@users.noreply.github.com>
2023-10-24 10:22:30 -0400

    Update datetime_convert.py

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,7 +1,13 @@
 from datetime import datetime
 
+# make the date and time a string
 date_str = "2022-03-17 10:45:30"
+
+# make an object that takes strips the string
 date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+
+#format the date and time
 formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
 
+#print the formatted date
 print(formatted_date)
