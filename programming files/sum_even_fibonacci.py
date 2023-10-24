8328058c42a8a2433c102c1cf8a6f3af5163c494
kpomroy <97473278+kpomroy@users.noreply.github.com>
2023-10-24 10:38:12 -0400

    Update sum_even_fibonacci.py

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -3,9 +3,9 @@ a, b = 0, 1
 sum_even = 0
 
 #While b is less than 4000000 run the loop to sum all even integers below 
-while b < 4000000:
+while b > 4000000:
     #Checking if b is even
-    if b % 2 == 0:
+    if b % 2 != 0:
         #Summming even values
         sum_even += b
     #Increasing variables
