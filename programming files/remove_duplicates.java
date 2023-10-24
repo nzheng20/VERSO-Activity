77b152c35238e23a7911249d94e84a07bfdda548
mason-eschleman <143549737+mason-eschleman@users.noreply.github.com>
2023-10-24 10:28:41 -0400

    Update remove_duplicates.java

--- programming files/remove_duplicates.java
+++ programming files/remove_duplicates.java
@@ -5,7 +5,11 @@ import java.util.List;
 public class remove_duplicates {
 
     public static List<Integer> removeDuplicates(List<Integer> arr, List<Integer> removedElements) {
+
+        //initialize new list
         List<Integer> uniqueArr = new ArrayList<>();
+
+        //loop through the list and check for duplicates, remove duplicates
         for (int i = 0; i < arr.size(); i++) {
             if (!uniqueArr.contains(arr.get(i))) {
                 uniqueArr.add(arr.get(i));
@@ -16,11 +20,14 @@ public class remove_duplicates {
         return uniqueArr;
     }
 
+    //main
     public static void main(String[] args) {
         // Test the function
         List<Integer> arr = Arrays.asList(1, 2, 2, 3, 4, 4, 5);
         List<Integer> removedElements = new ArrayList<>();
         List<Integer> uniqueArr = removeDuplicates(arr, removedElements);
+
+        //display new lists after duplicate removal
         System.out.println("Original array: " + arr);
         System.out.println("Unique array: " + uniqueArr);
         System.out.println("Removed elements: " + removedElements);
