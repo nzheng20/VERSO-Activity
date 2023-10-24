38ce1b9cf11740680619db171f536fe34f1933ed
kpomroy <97473278+kpomroy@users.noreply.github.com>
2023-10-24 10:22:31 -0400

    Commented datetime_convert.cc

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -5,14 +5,24 @@
 
 int main()
 {
+    // Setting the date string
     std::string date_str = "2022-03-17 10:45:30";
+
+    // Creating a struct to store the parsed date and initializing it
     std::tm date_obj = {};
+
+    // Creating a string stream and parsing the date string into a std::tm structure
     std::istringstream ss(date_str);
     ss >> std::get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
+
+    // Creating another string stream to format the date
     std::stringstream formatted_date_ss;
     formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
+
+    // Converting the formatted date back to a string
     std::string formatted_date = formatted_date_ss.str();
 
+    // Printing the formatted date
     std::cout << formatted_date << std::endl;
 
     return 0;
