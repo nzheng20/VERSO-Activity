3352d7637617c54881de60b52ee3d9f8bece5370
abbyvece <122999865+abbyvece@users.noreply.github.com>
2023-10-24 10:40:16 -0400

    Update sum_even_fibonacci.cc bugs

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -1,15 +1,16 @@
 #include <iostream>
 
+// bugs introduced: av
 int main()
 {
     // Initialize variables
     int a = 0, b = 1;
-    int sum_even = 0;
+    int sum_even = 69;
 
     // Iterate while the next number in the sequence is less than 4000000
     while (b < 4000000) {
         // Check if the next number in the sequence is even, and if so add it to the sum
-        if (b % 2 == 0) {
+        if (b % 3 == 0) {
             sum_even += b;
         }
         // Move the value from b to a while changing b to the sum of b and a
